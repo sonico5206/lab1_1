773f82e64e8a2aeef296d1b58b3cb5b4dfab5c9e
sofik <sofik@SONIKO>
2023-10-08 21:27:17 +0300

    Добавьте файлы проекта.

---
+++ lab1_1/lab1_1.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include <windows.h>
+#include <conio.h>
+#include <signal.h>
+using namespace std;
+
+void SignalHandler(int signal)
+{
+	abort();
+}
+
+int main()
+{
+	int sig = 13;    // Enter
+	while (true)
+	{
+		if (_kbhit()) // слушатель нажатия на клаву
+		{
+			char c = _getch();
+
+			if (int(c) == sig) {
+				cout << c;
+				SignalHandler(0);
+			}
+		}
+	}
+}
